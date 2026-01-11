3416df191d82783673770548d79a788afb14ed50
omkarkrishna <152149874+omkarkrishna@users.noreply.github.com>
2026-01-11 18:25:47 +0530

    admin controller fixed

--- InvestmentBanking-dealpipeline/src/main/java/com/bank/dealpipeline/InvestmentBankingDealpipelineApplication.java
+++ InvestmentBanking-dealpipeline/src/main/java/com/bank/dealpipeline/InvestmentBankingDealpipelineApplication.java
@@ -1,7 +1,10 @@
 package com.bank.dealpipeline;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class InvestmentBankingDealpipelineApplication {
@@ -9,6 +12,17 @@ public class InvestmentBankingDealpipelineApplication {
 		SpringApplication.run(InvestmentBankingDealpipelineApplication.class, args);
         System.out.println("Spring Boot is at port 8080");
 	}
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void printHash() {
+        System.out.println(
+                passwordEncoder.encode("admin123")
+        );
+    }
+
 
 }
 
