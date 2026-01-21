9fa41c19d689cdd76f1208f45a4fec74d22d7e88
omkarkrishna <152149874+omkarkrishna@users.noreply.github.com>
2026-01-21 07:11:26 +0530

    final commit

--- frontend/src/deals/DealBoard.tsx
+++ frontend/src/deals/DealBoard.tsx
@@ -35,7 +35,7 @@ const DealBoard = () => {
         }}
       >
         <Typography variant="h4" fontWeight={600}>
-          Deal Pipelin
+          Deal Pipeline
         </Typography>
         <CreateDealDialog />
       </Box>
